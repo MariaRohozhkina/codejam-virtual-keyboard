52c4e72cae6b22b6425d8f60ef3519f7fde0420e
MariaRohozhkina <mariaspirit228@gmail.com>
2019-11-06 22:02:37 +0300

    fix: layout

--- script.js
+++ script.js
@@ -191,20 +191,3 @@ window.addEventListener('keydown', (event) => {
   }
 });
 
-
-const language = localStorage.getItem('button[i].className');
-window.onload = function loading() {
-  const keys = document.getElementsByClassName('key');
-
-  for (let i = 0; i < keys.length; i += 1) {
-    keys[i].className = language;
-  }
-
-  let p = 0;
-  if (keys[26].className == 'key english' || keys[26].className == 'key english english' || keys[26].className == 'key english english english' || keys[26].className == 'key english english english english') {
-    for (let o = 14; o < keys.length; o += 1) {
-      keys[o].innerText = arrLine2En[p];
-      p += 1;
-    }
-  }
-};
